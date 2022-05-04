c20ab6b8bdbe20deae2346a14e127fe8c024f43f
Kristjan <krix@planet.ee>
2022-05-04 23:18:17 +0300

    moved partial full-bullet loan logic to separate file

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,140 +1,24 @@
-import React, { useEffect, useState } from "react"
-import styled from "styled-components"
-
 //redux
-import { useSelector, useDispatch } from "react-redux"
-import {
-  setLoanAmount,
-  setInterestRate,
-  setStartDate,
-  setEndDate,
-  setDaysPerYear,
-} from "../slices/loanSlice"
+import { useSelector } from "react-redux"
 
-import parseISO from "date-fns/parseISO"
-import isAfter from "date-fns/isAfter"
+import { LoanTypes } from "../enums.ts"
 
-import Button from "@mui/material/Button"
-import Grid from "@mui/material/Grid"
-import TextField from "@mui/material/TextField"
-import AdapterDateFns from "@mui/lab/AdapterDateFns"
-import LocalizationProvider from "@mui/lab/LocalizationProvider"
-import MobileDatePicker from "@mui/lab/MobileDatePicker"
-
-import enGBLocale from "date-fns/locale/en-GB"
-
-const localeMap = {
-  enGB: enGBLocale,
-}
-
-const maskMap = {
-  enGB: "__/__/____",
-}
+import { FullBullet } from "../components"
 
 const InputContainer = ({ handleClick }) => {
   //using redux
-  const {
-    loanAmount,
-    interestRate,
-    daysPerYear,
-    startDate: start,
-    endDate: end,
-  } = useSelector((state) => state.loan)
+  const { loanType } = useSelector((state) => state.type)
 
-  const startDate = parseISO(start)
-  const endDate = parseISO(end)
-
-  const dispatch = useDispatch()
-
-  const [validSelection, setValidSelection] = useState(true) //is loan start date before end date?
-
-  const isEndDateValid = (startDate, endDate) => {
-    let valid = isAfter(endDate, startDate)
-    return valid
+  switch (loanType) {
+    case LoanTypes.Balloon:
+      return <h1>Baloon Loan Type</h1>
+    case LoanTypes.Bullet:
+      return <h1>Bullet Loan Type</h1>
+    case LoanTypes.FullBullet:
+      return <FullBullet handleClick={handleClick} />
+    default:
+      break
   }
-
-  useEffect(() => {
-    if (startDate !== null && endDate !== null) {
-      setValidSelection(isEndDateValid(startDate, endDate))
-    }
-  }, [startDate, endDate])
-
-  return (
-    <Wrapper>
-      <LocalizationProvider
-        dateAdapter={AdapterDateFns}
-        locale={localeMap["fr"]}
-      >
-        <Grid container spacing={1}>
-          <Grid item md={3}>
-            <TextField
-              value={loanAmount}
-              onChange={(e) => dispatch(setLoanAmount(e.target.value))}
-              label="Loan amount €"
-              type="number"
-            />
-          </Grid>
-          <Grid item md={3}>
-            <TextField
-              value={interestRate}
-              onChange={(e) => dispatch(setInterestRate(e.target.value))}
-              label="Interest rate %"
-              type="number"
-            />
-          </Grid>
-          <Grid item md={3}>
-            <TextField
-              value={daysPerYear}
-              onChange={(e) => dispatch(setDaysPerYear(e.target.value))}
-              label="Days per year"
-              type="number"
-            />
-          </Grid>
-          <Grid item md={3}>
-            <MobileDatePicker
-              label="Loan start date"
-              value={startDate}
-              mask={maskMap["fr"]}
-              onChange={(newValue) => {
-                dispatch(setStartDate(newValue.toISOString()))
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </Grid>
-          <Grid item md={3}>
-            <MobileDatePicker
-              label="Loan end date"
-              value={endDate}
-              className={!validSelection ? "error" : ""}
-              onChange={(newValue) => {
-                dispatch(setEndDate(newValue.toISOString()))
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  helperText={!validSelection ? "Date cannot be earlier!" : ""}
-                />
-              )}
-            />
-          </Grid>
-          <Grid item md={3}>
-            <Button variant="contained" onClick={() => handleClick()}>
-              Calculate
-            </Button>
-          </Grid>
-        </Grid>
-      </LocalizationProvider>
-    </Wrapper>
-  )
 }
-
-const Wrapper = styled.div`
-  margin-top: 1rem;
-  background-color: white;
-  padding: 1rem;
-  .error {
-    border-color: red;
-  }
-`
 
 export default InputContainer
