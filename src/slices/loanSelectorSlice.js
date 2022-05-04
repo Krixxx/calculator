12bc9152d87472f86f4e9c06bb98564744795ff7
Kristjan <krix@planet.ee>
2022-05-04 20:49:45 +0300

    added loan selector slice for redux

---
+++ src/slices/loanSelectorSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { LoanTypes } from "../enums"
+
+const initialState = {
+  loanType: LoanTypes.FullBullet,
+}
+
+export const loanSelectorSlice = createSlice({
+  name: "type",
+  initialState,
+  reducers: {
+    setLoanType: (state, action) => {
+      state.loanType = action.payload
+    },
+  },
+})
+
+export const { setLoanType } = loanSelectorSlice.actions
+
+export default loanSelectorSlice.reducer
