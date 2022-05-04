387cd6a1ad4bf5df6472e74fe22190a94bb29a8e
Kristjan <krix@planet.ee>
2022-05-04 23:18:36 +0300

    added new reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
+import loanSelectorSlice from "../slices/loanSelectorSlice"
 import loanReducer from "../slices/loanSlice"
 
 export const store = configureStore({
   reducer: {
     loan: loanReducer,
+    type: loanSelectorSlice,
   },
 })
